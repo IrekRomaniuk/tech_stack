e06031105f46db14acd76297d050dbc7b2724b52
Irek Romaniuk <irekromaniuk@Ireks-MacBook-Air.local>
2017-07-01 09:01:48 -0400

    comment console

--- src/reducers/SelectionReducer.js
+++ src/reducers/SelectionReducer.js
@@ -2,7 +2,7 @@ export default (state = null, action) => {
     //console.log(action);
     switch (action.type) {
         case 'select_library':
-            console.log(action.payload);
+            //console.log(action.payload);
             return action.payload;
         default: 
             return state;
